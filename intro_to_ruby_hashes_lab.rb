0bac2455ec8ddf165e5c2871e40e76939f64366d
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-02 17:13:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
       :railroads => {
         :pieces => 4,
         :rent_in_dollars => {
-          :one_piece_owned => ,
+          :key1 => ,
           :key2 => ,
           :key3 => ,
           :key4 =>
