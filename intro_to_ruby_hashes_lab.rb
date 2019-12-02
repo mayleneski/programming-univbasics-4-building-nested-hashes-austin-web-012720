6675476f18053e2149d6691d3efdc220259465b3
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-02 17:20:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,8 @@ def monopoly_with_second_tier
       :railroads => {
         :pieces => 4
         :rent_in_dollars => {
-          :one_piece_owned => 25
+          :one_piece_owned => 25,
+          :two_pieces_owned => 50
         }
     }
   }
