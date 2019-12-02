f09d87e7a3cbc32d86e235c1aa379842deaad6a9
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-02 17:27:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,8 @@ def monopoly_with_second_tier
           :four_pieces_owned => 200
         },
         :names => {
-          :reading_railroad => {}
+          :reading_railroad => {},
+          :pensylvania_railroad => {}
         }
     }
   }
